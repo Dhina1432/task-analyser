61a9253c8052422c8930c576934833087ffa3a9a
Dhina@1432 <dhinadhinagaraj1432@gmail.com>
2025-12-01 13:32:24 +0530

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://task-analyzer-l6sh.onrender.com";
 
 const taskForm = document.getElementById("task-form");
 const bulkJsonInput = document.getElementById("bulk-json");
